650f321b2985a4c64ba113873de78d432f092224
mfarrell <michael.farrell@wexinc.com>
2020-04-13 19:10:52 -0500

    LWC for related tasks

--- src/lwc/relatedActivities/relatedActivities.js
+++ src/lwc/relatedActivities/relatedActivities.js
@@ -2,8 +2,42 @@
  * Created by mfarrell on 4/9/20.
  */
 
-import {LightningElement} from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getAllActivities from '@salesforce/apex/LWC_ActivitiesController.getAllActivities';
 
-export default class RelatedActivities extends LightningElement {
+export default class ActivityList extends NavigationMixin(LightningElement) {
+    activities;
+    error;
+
+    @api recordId;
+    
+    @wire(getAllActivities, {recordId: '$recordId'})
+    wiredActivities({error, data}) {
+        if (data) {
+            this.activities = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.activities = undefined;
+        }
+    }
+
+    navigateToRecordViewPage(eventRecordId) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: eventRecordId,
+                objectApiName: 'Task',
+                actionName: 'view'
+            }
+        });
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.navigateToRecordViewPage(event.target.dataset.recordId);
+    }
 
 }
